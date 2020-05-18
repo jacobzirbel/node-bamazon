15175db3d0d694f4a81c11aa471e9df4cc0c382e
Jacob Zirbel <jacobzirbel@gmail.com>
2020-05-18 17:48:11 -0500

    finish supervisor

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -9,7 +9,7 @@ function main() {
 	try {
 		connection.connect(function (err) {
 			if (err) throw err;
-			console.log("connected as id " + connection.threadId);
+			// console.log("connected as id " + connection.threadId);
 			startSupervisor();
 		});
 	} catch (err) {
@@ -17,7 +17,57 @@ function main() {
 	}
 }
 
-function startSupervisor() {}
+function startSupervisor() {
+	let actions = [
+		{ name: "View Departments", value: viewSalesByDepartment },
+		{ name: "Create New Department", value: createDepartment },
+	];
+	inquirer
+		.prompt([
+			{
+				name: "action",
+				message: "What would you like to do?",
+				type: "list",
+				choices: actions,
+			},
+			{
+				name: "name",
+				message: "Name of department?",
+				when: (answers) => answers.action === createDepartment,
+			},
+
+			{
+				name: "cost",
+				message: "Overhead Cost?",
+				when: (answers) => answers.action === createDepartment,
+			},
+		])
+		.then((response) => response.action(response.name, response.cost));
+}
+
+function createDepartment(departmentName, overheadCosts) {
+	let query = `INSERT INTO departments (department_name, overhead_costs)
+	values ('${departmentName}', ${overheadCosts})`;
+	sqlQuery(query)
+		.catch(() => console.log(highlight("There was an error")))
+		.finally(startSupervisor);
+}
+
+function viewSalesByDepartment() {
+	let query = `SELECT d.department_id, d.department_name, d.overhead_costs, 
+		sum(p.sold_quantity*p.price) AS total_sales, 
+		sum(p.sold_quantity*p.price) - d.overhead_costs as total_profit
+	FROM departments d, products p
+	WHERE d.department_name = p.department_name
+	GROUP BY d.department_id, d.department_name, d.overhead_costs
+	ORDER BY d.department_id;`;
+	sqlQuery(query)
+		.then((res) => {
+			console.table(res);
+		})
+		.catch(() => console.log(highlight("There was an error")))
+		.finally(startSupervisor);
+}
 
 function sqlQuery(query, vars = {}) {
 	return new Promise((resolve, reject) => {
@@ -32,23 +82,3 @@ function sqlQuery(query, vars = {}) {
 function highlight(words) {
 	return `\n**************\n${words}\n**************\n`;
 }
-
-function numberValidator(amount) {
-	if (amount > 1000000 || amount < 1 || isNaN(amount)) {
-		console.log(highlight("Invalid Number"));
-		return false;
-	}
-	return true;
-}
-
-/* 
-GET PRODUCT SALES and PROFIT BY DEPARTMENT: 
-
-SELECT d.department_id, d.department_name, d.overhead_costs, sum(p.sold_quantity*p.price) AS total_sales, 
-sum(p.sold_quantity*p.price) - d.overhead_costs as total_profit
-FROM departments d, products p
-WHERE d.department_name = p.department_name
-GROUP BY d.department_id, d.department_name, d.overhead_costs
-ORDER BY d.department_id;
-
-*/
